29acb767d537ab92d69b5fa8bc76163fb187efdf
huyen99 <huyenyp01101999@gmail.com>
2021-09-08 08:55:45 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback } from "react";
 
 function App() {
+  const listStudent = ["Huyen", "Hoa", "Hung", "Long"];
+
+  const useAction = () => {
+    const [position, setPosition] = useState(0);
+
+    const increase = useCallback(() => {
+      if (position !== listStudent.length - 1) setPosition(position + 1);
+      else setPosition(0);
+    }, [position]);
+    const decrease = useCallback(() => {
+      if (position !== 0) setPosition(position - 1);
+      else setPosition(listStudent.length - 1);
+    }, [position]);
+
+    return [position, { increase, decrease }];
+  };
+
+  const [position, { increase, decrease }] = useAction();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="list-student">
+        <p>学生一覧：[{listStudent.toString()}]</p>
+      </div>
+      <div className="position">位置：{position + 1}</div>
+      <div className="name">
+        <p>名前：{listStudent[position]}</p>
+      </div>
+      <div className="action">
+        <button className="action-increase" onClick={increase}>
+          次に
+        </button>
+        <button onClick={decrease}>前に</button>
+      </div>
     </div>
   );
 }
